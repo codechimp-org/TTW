4c62e2eb98610784aea20101365c3826b6726977
Andrew-Codechimp <andrew@codechimp.org>
2015-07-01 16:28:07 +0100

    Work on intro screen

--- mobile/src/main/java/org/codechimp/ttw/MainActivity.java
+++ mobile/src/main/java/org/codechimp/ttw/MainActivity.java
@@ -14,8 +14,6 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button hideLauncherButton;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(false);
         }
 
-        hideLauncherButton = (Button) findViewById(R.id.hideLauncherButton);
+        Button hideLauncherButton = (Button) findViewById(R.id.hideLauncherButton);
         hideLauncherButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -42,28 +40,4 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
 }
