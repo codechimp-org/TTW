7c0f8f38bee0d5e5f34343027bdc9777e646048a
Andrew <andrew@codechimp.org>
2018-01-10 09:37:29 +0000

    Fix dependencies

--- mobile/src/main/java/org/codechimp/ttw/MainActivity.java
+++ mobile/src/main/java/org/codechimp/ttw/MainActivity.java
@@ -9,16 +9,10 @@ import android.os.Handler;
 import android.os.ResultReceiver;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
